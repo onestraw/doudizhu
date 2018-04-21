ba71dda99341a887e719b525ce2a04fd73872ee4
onestraw <hexiaowei91@163.com>
2018-04-21 11:38:40 +0800

    update the release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='doudizhu',
-    version='0.1.2',
+    version='0.1.3',
     description='doudizhu engine',
     long_description=readme,
     url='https://github.com/onestraw/doudizhu',
